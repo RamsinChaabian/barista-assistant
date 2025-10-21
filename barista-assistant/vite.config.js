192261251bc424e9963e711008e8110d84bb2f32
ramsinchaabian <ramsin.chaabian@gmail.com>
2025-10-21 14:26:37 +0330

    update

--- barista-assistant/vite.config.js
+++ barista-assistant/vite.config.js
@@ -1,11 +1,10 @@
 // vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // این خط را اصلاح کنید
-  base: '/barista-assistant/', 
+  // مسیر پایه برای دامنه سفارشی به ریشه تغییر کرد
+  base: '/', 
 })
